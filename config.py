2e18b13ea803b3c0abe7ad4f628aa10c65fbe0a0
botwalain <167398125+botwalain@users.noreply.github.com>
2024-05-17 17:53:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "<b>Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link</b>")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "1936055439").split()):
+    for x in (os.environ.get("ADMINS", "1936055439 5356411463").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
