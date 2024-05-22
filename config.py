095722e680d888dc355e90820229749e36c2bd60
botwalain <167398125+botwalain@users.noreply.github.com>
2024-05-22 15:32:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -62,7 +62,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "True") == "True" el
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "Join @DelightRedirect for latest updates"
+USER_REPLY_TEXT = "Join @DelightRedirect for latest updates."
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(6497757690)
